7cb25e7077117745b3a6d2049cd90af15f2e7f18
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2020-12-11 14:04:26 +0900

    Update 2522.cpp

--- boj./c++/2522.cpp
+++ boj./c++/2522.cpp
@@ -5,7 +5,7 @@ int main(){
     int n=0;
     scanf("%d",&n);
 
-    for(int i=0;i<=2*n;++i){
+    for(int i=1;i<=2*n-1;++i){
         for(int j=1;j<=abs(n-i);++j)
             printf(" ");
         for(int j=1;j<=n-abs(n-i);++j){
@@ -14,3 +14,19 @@ int main(){
     }
     return 0;
 }
+/*#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int n=0;
+    scanf("%d",&n);
+
+    for(int i=0;i<=2*n;++i){
+        for(int j=1;j<=abs(n-i);++j)
+            printf(" ");
+        for(int j=1;j<=n-abs(n-i);++j){
+            printf("*");
+        }printf("\n");
+    }
+    return 0;
+}*/
