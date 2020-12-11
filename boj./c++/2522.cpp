ea90597f61f213c36d5c2c47881be2a36428254e
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2020-12-11 14:02:42 +0900

    Create 2522.cpp

---
+++ boj./c++/2522.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int n=0;
+    scanf("%d",&n);
+
+    for(int i=0;i<=2*n;++i){
+        for(int j=1;j<=abs(n-i);++j)
+            printf(" ");
+        for(int j=1;j<=n-abs(n-i);++j){
+            printf("*");
+        }printf("\n");
+    }
+    return 0;
+}
