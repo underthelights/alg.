08d65ca055450affc0b3987b13f17ce04d372d73
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-02-14 00:01:18 +0900

    Create 5337.cpp

---
+++ boj./c++/5337.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+using namespace std;
+#define O ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+int main() {
+    O;
+    cout<<".  .   .\n";
+    cout<<"|  | _ | _. _ ._ _  _\n";
+    cout<<"|/\\|(/.|(_.(_)[ | )(/.\n";
+
+    return 0;
+}
