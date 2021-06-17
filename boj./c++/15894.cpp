2277d2cfcdc7a5a402fab6ba12d86f96e1bdf32e
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-06-17 21:13:43 +0900

    Create 15894.cpp

---
+++ boj./c++/15894.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+#define O ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+int main(){
+    O;
+    unsigned int N;
+    cin>>N;
+    cout<<4*N;
+    return 0;
+}
+
