326fd1cca579b884cddb9bdba3624a0d89ef9f92
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-06-17 21:15:09 +0900

    Create 16394.cpp

---
+++ boj./c++/16394.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+#define O ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+int main(){
+    O;
+    unsigned int N;
+    cin>>N;
+
+    cout << N - 1946 << "\n";
+    return 0;
+}
