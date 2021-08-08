1edaea4857b9546e8a325fa80cd7ce93d9921d37
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-08-08 23:38:29 +0900

    Create basic.cpp

---
+++ Code Snippets/basic.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+#define O ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define LL long long
+
+int main(){
+    O;
+    return 0;
+}
